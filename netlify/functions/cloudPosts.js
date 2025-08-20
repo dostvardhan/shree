4787aca30af23ae5016edb0ac285f74fd10635c3
dostvardhan <mitravardhan@gmail.com>
2025-08-20 21:25:02 +0530

    Create cloudPosts.js

---
+++ netlify/functions/cloudPosts.js
@@ -0,0 +1,5 @@
+// netlify/functions/cloudPosts.js
+// Placeholder data for Nadaniya gallery
+module.exports = [
+  { id: 1, title: "Welcome Post", content: "This is a placeholder post for Nadaniya gallery." }
+];
