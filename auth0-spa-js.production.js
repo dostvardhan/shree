9c8a34781e6e4f138be199c8449c56aa4efe76c0
Mitravardhan Mishra <mitravardhan@gmail.com>
2025-09-16 20:41:25 +0530

    chore: add vendor auth0-spa-js and local-first auth-init loader

---
+++ auth0-spa-js.production.js
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error><Code>AccessDenied</Code><Message>Access Denied</Message><RequestId>JCZC0N19Q1PYYJZ4</RequestId><HostId>8WQtSlryR21b5XM8kP0fG+7LQsd7vbIVsiUTwci+XGwwLDyOjwMK4RsgzLrPI0oyXGKig41KnX0=</HostId></Error>
